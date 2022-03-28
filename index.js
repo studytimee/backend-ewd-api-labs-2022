6e44c12e03357ab5885503e40ac02f23cb93a709
Abid Ali <studytimee@gmail.com>
2022-03-28 22:47:16 +0100

    Linting - Configured Linting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config()
 const port = process.env.PORT
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  req.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello EWD ');
 });
 
